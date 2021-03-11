878586d20326480dd50f4aec86ba01adfd716d10
Daan de Klein <0085940@rocdeleijgraaf.nl>
2021-03-11 13:59:20 +0100

    begin van de site

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="nl">
+    <head>
+        <meta charset="utf-8">
+        <title>Surf Shack</title>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Surfboarden</a></li>
+                <li><a href="#">Item Shop</a></li>
+                <li><a href="#">Surflessen</a></li>
+                <li><a href="#">Surfticket</a></li>
+                <li><a href="#">Info</a></li>
+                <li><a href="#" id="winkelwagen">Winkelwagen</a></li>
+            </ul>
+        </nav>
+    </header>
+<body>
+
+</body>
+</html>
