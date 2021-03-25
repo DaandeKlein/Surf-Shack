4a0f2e348c39e4926c4a418997754d582cc742cc
Romano Dekkers <0085420@rocdeleijgraaf.nl>
2021-03-25 10:25:16 +0100

    updates

--- info.php
+++ info.php
@@ -8,23 +8,19 @@
 		<style>
 		p {
                 color: white;
-                font-size: 28px;
-                text-align: left;
+                font-size: 24px;
+                text-align: centre;
                 padding-left: 70px;
 				}
-        #overons {
-                padding-top: 30px;
-				}
-        #bericht {
-                padding-top: 140px;
+		#titel {
+				text-align: center;
 				}
 		</style>
 	</head>
-	<body>
 	<header>
         <nav>
             <ul>
-				<li><img src="logo.png" alt="logo" height="55px" width="55px" id="logo"></a></li>
+				<li><img src="logo.png" alt="logo" height="55px" width="55px" id="logo"></li>
                 <li><a href="index.php" class="nav">Home</a></li>
                 <li><a href="contact.php" class="nav">Contact</a></li>
                 <li><a href="surfboarden.php" class="nav">Surfboarden</a></li>
@@ -36,5 +32,7 @@
             </ul>
         </nav>
     </header>
+	<body>
+	<p>test</p>
 	</body>
 </html>
