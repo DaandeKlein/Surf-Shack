2e25b040e13d2bb0dd62cdd4c05d83f16a873627
Romano Dekkers <0085420@rocdeleijgraaf.nl>
2021-04-01 11:03:46 +0200

    bababooey

--- surfticket.php
+++ surfticket.php
@@ -1,1 +1,46 @@
-"hoi" 
+<html lang="nl">
+    <head>
+        <meta charset="utf-8">
+        <title>Surf Shack</title>
+        <link rel="stylesheet" href="surfticket.css">
+		<style>
+		p {
+                font-size: 24px;
+                text-align: centre;
+				}
+		#titel {
+				text-align: centre;
+				}
+		</style>
+	</head>
+<body>
+   <header>
+   <div id="logo">
+	<img src="logo.png" alt="het logo">
+   </div>
+<div class="menu">
+        <nav>
+            <ul>
+                <li><a href="index.php" class="nav">Home</a></li>
+                <li><a href="contact.php" class="nav">Contact</a></li>
+                <li><a href="surfboarden.php" class="nav">Surfboarden</a></li>
+                <li><a href="itemshop.php" class="nav">Item Shop</a></li>
+                <li><a href="surflessen.php" class="nav">Surflessen</a></li>
+                <li><a href="surfticket.php" class="nav">Surfticket</a></li>
+                <li><a href="info.php" class="nav">Info</a></li>
+              
+           </ul>
+            <div id="winkelwagen">
+                <a href="winkelwagen.php" id="winkelwagen" target="blank_"><img src="winkelwagen.png" alt="winkelwagen"></a>
+            </div>
+        </nav>
+    </div>
+	</header>
+	<div class="card">
+	<img src="indoor1.jpg" alt="PepSports" style="width:100%">
+	<h1>Indoor surfplek</h1>
+	<p class="price">$19.99 p.p</p>
+	<p>hier komt info over de plek</p>
+</div>
+</body>
+</html>
