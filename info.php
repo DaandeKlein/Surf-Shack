182df40e736e76c6f04d94ac6e6ea4dec80de543
Daan de Klein <0085940@rocdeleijgraaf.nl>
2021-04-01 09:32:00 +0200

    hij doet het weer

--- info.php
+++ info.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 
 <html lang="nl">
-    <head>
-        <meta charset="utf-8">
-        <title>Surf Shack</title>
-        <link rel="stylesheet" href="new.css">
-		<style>
-		p {
-                font-size: 24px;
-                text-align: centre;
-				}
-		#titel {
-				text-align: center;
-				}
-		</style>
-	</head>
+<head>
+    <meta charset="utf-8">
+    <title>Surf Shack</title>
+    <link rel="stylesheet" href="new.css">
+    <style>
+        p {
+            font-size: 24px;
+            text-align: center;
+        }
+        #titel {
+            text-align: center;
+        }
+    </style>
+</head>
 <body>
-   <header>
-   <div id="logo">
-	<img src="logo.png" alt="het logo">
-   </div>
-<div class="menu">
+<header>
+    <div id="logo">
+        <img src="logo.png" alt="het logo">
+    </div>
+    <div class="menu">
         <nav>
             <ul>
                 <li><a href="index.php" class="nav">Home</a></li>
@@ -30,18 +30,18 @@
                 <li><a href="surflessen.php" class="nav">Surflessen</a></li>
                 <li><a href="surfticket.php" class="nav">Surfticket</a></li>
                 <li><a href="info.php" class="nav">Info</a></li>
-              
-           </ul>
+
+            </ul>
             <div id="winkelwagen">
                 <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" id="winkelwagen" target="blank_"><img src="winkelwagen.png" alt="winkelwagen"></a>
             </div>
         </nav>
     </div>
-	</header>
-	<div id="content">
-	<h1>Ons bedrijf</h1>
-	<p>Dit bedrijf is opgericht in 2021 door 3 jongens genaamd Romano, Daan en Valentino. Ons bedrijf is opgericht om kwaliteit spullen en goede informatie te leveren over surfen. Wij geven informatie over de beste plekken om te gaan buiten/binnen surfen. Wij leveren kwaliteit surfborden, wetsuits en surftickets die kunnen worden gebruikt om ergens te gaan binnensurfen! U kunt ook gebuik maken van onze map waar de beste plekken op staan on te gaan surfen.</p>
-	<img src="surfboard4.jpg" alt="surfen">
-	</div>
-	</body>
+</header>
+<div id="content">
+    <h1>Ons bedrijf</h1>
+    <p>Dit bedrijf is opgericht in 2021 door 3 jongens genaamd Romano, Daan en Valentino. Ons bedrijf is opgericht om kwaliteit spullen en goede informatie te leveren over surfen. Wij geven informatie over de beste plekken om te gaan buiten/binnen surfen. Wij leveren kwaliteit surfborden, wetsuits en surftickets die kunnen worden gebruikt om ergens te gaan binnensurfen! U kunt ook gebuik maken van onze map waar de beste plekken op staan on te gaan surfen.</p>
+    <img src="surfboard4.jpg" alt="surfen">
+</div>
+</body>
 </html>
